c078772a2029758a5d073ae7d8cf344f6eba3fbc
salmankavanur <mpsalman2011@gmail.com>
2025-04-11 16:48:38 +0530

    error fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
 
   // typescript: {
   //   ignoreBuildErrors: true, // Ignores TypeScript errors during build
